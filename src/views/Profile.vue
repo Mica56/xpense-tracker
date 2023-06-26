5a9d5256d53cef128a4fc8fbd6c504f624ac8249
Aaron Santillan <aaronsantillandev@gmail.com>
2023-06-26 22:15:11 +0800

    my first commit

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -19,7 +19,12 @@
                     <img src="../assets/images/profile.png">
                 </div>
                 <h2 class="account-name">Misty Williams</h2>
-                <span class="username">@mistywilliams96</span> <br> <br>
+                <span class="username">@mistywilliams96</span>
+                <div class="profile">
+                  <p><span class="email">Email: </span>mistywilliams@gmail.com</p> 
+                  <p><span class="number">Phone number: </span>09389140463 </p> 
+                  <p><span class="account-type">Account type: </span> Normal</p>
+                </div>
 
                 <hr style="width:350px">
                 
