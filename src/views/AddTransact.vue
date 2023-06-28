53bd98d01cf9d1a38e529e7789364c01bf315357
Mica56 <tayotomichael18@gmail.com>
2023-06-29 07:17:54 +0800

    Add transact, home, and login changes

--- src/views/AddTransact.vue
+++ src/views/AddTransact.vue
@@ -27,29 +27,36 @@
                 <input type="date" class="inputfield">
                 <label>Expense Type</label>
                 <div class="dropdown">
-                    <div id="selectfield-expense">
-                    <p id="selecttext-expense">Select Expense Type</p>
-                    <font-awesome-icon :icon="['fas', 'chevron-down']" size="xs"/>
+                    <!-- <div id="selectfield-expense">
+                        <p id="selecttext-expense">Select Expense Type</p>
+                        <font-awesome-icon :icon="['fas', 'chevron-down']" size="xs"/>
                     </div>
                     <ul id="list-expense" class="hide1">
-                    <li class="options-expense">
-                        Food
-                    </li>
-                    <li class="options-expense">
-                        Transfer Payments
-                    </li>
-                    <li class="options-expense">
-                        Travel Expenses
-                    </li>
-                    <li class="options-expense">
-                        Load Transaction
-                    </li>
-                    </ul>
+                        <li class="options-expense">
+                            Food
+                        </li>
+                        <li class="options-expense">
+                            Transfer Payments
+                        </li>
+                        <li class="options-expense">
+                            Travel Expenses
+                        </li>
+                        <li class="options-expense">
+                            Load Transaction
+                        </li>
+                    </ul> -->
+                    <select id="selectfield-expense" v-model="selected_ET" @input="storeExpType(selected_ET)">
+                        <option value="" id="selecttext-expense" disabled selected>Select Expense Type</option>
+                        <font-awesome-icon :icon="['fas', 'chevron-down']" size="xs"/>
+                        <option id="list-expense" v-for="option in optionsExpense" :key="option.value">
+                            {{option.name}}
+                        </option>
+                    </select>
                 </div>
 
                 <label>Transaction Type</label>
                 <div class="dropdown">
-                    <div id="selectfield-transaction">
+                    <!-- <div id="selectfield-transaction">
                     <p id="selecttext-transaction">Select Transaction Type</p>
                     <font-awesome-icon :icon="['fas', 'chevron-down']" size="xs"/>
                     </div>
@@ -60,8 +67,16 @@
                     <li class="options-transaction">
                         Credit
                     </li>
-                    </ul>
+                    </ul> -->
+                    <select id="selectfield-transaction" v-model="selected_TT" @input="storeTransType(selected_TT)">
+                        <option value="" id="selecttext-transaction" disabled selected>Select Expense Type</option>
+                        <font-awesome-icon :icon="['fas', 'chevron-down']" size="xs"/>
+                        <option id="list-transaction" v-for="option in optionsTransact" :key="option.value">
+                            {{option.name}}
+                        </option>
+                    </select>
                 </div>
+                    <!-- remove this router-link after fixing transaction list -->
                     <router-link to="/transexpense" style="text-decoration: none;color: inherit;">
                         <input type="submit" class="buttone" value="SAVE">
                     </router-link>
@@ -86,60 +101,84 @@ export default {
     name: 'AddTransaction',
     components: {
         NavMenu,
+    },
+    data() {
+    return {
+        selected_ET: null,
+        optionsExpense: [
+            {"value": 'A', "name": "Food"},
+            {"value": 'B', "name": "Transfer Payments"},
+            {"value": 'C', "name": "Travel Expenses"},
+            {"value": 'D', "name": "Load Transaction"},
+        ],
+        selected_TT: null,
+        optionsTransact: [
+            {"value": 1, "name": "Debit"},
+            {"value": 2, "name": "Credit"},
+        ],
     }
-}
+    },
 
-// var selectField = document.getElementById("selectfield");
-// var selectText = document.getElementById("selecttext");
-// var options = document.getElementsByClassName("options");
-// var list = document.getElementById("list");
+    methods: {
+        storeExpType(selected_ET) {
+            console.log(selected_ET) 
+        },
+        storeTransType(selected_TT) {
+        console.log(selected_TT) 
+        },
+    },
+    // var selectField = document.getElementById("selectfield");
+    // var selectText = document.getElementById("selecttext");
+    // var options = document.getElementsByClassName("options");
+    // var list = document.getElementById("list");
 
-// selectField.onclick = function () {
-// list.classList.toggle("hide");
-// }
+    // selectField.onclick = function () {
+    // list.classList.toggle("hide");
+    // }
 
-// for (option of options) {
-// option.onclick = function () {
-//     var imageElement = document.createElement("img");
-//     selectText.innerHTML = "";
-//     selectText.innerHTML += this.innerHTML;
-//     list.classList.toggle("hide");
-// };
-// }
+    // for (option of options) {
+    // option.onclick = function () {
+    //     var imageElement = document.createElement("img");
+    //     selectText.innerHTML = "";
+    //     selectText.innerHTML += this.innerHTML;
+    //     list.classList.toggle("hide");
+    // };
+    // }
 
-// // FOR eXPENSE TYPE DROPDOWN
-// var selectFieldExpense = document.getElementById("selectfield-expense");
-// var selectTextExpense = document.getElementById("selecttext-expense");
-// var optionsExpense = document.getElementsByClassName("options-expense");
-// var listExpense = document.getElementById("list-expense");
+    // // FOR eXPENSE TYPE DROPDOWN
+    // var selectFieldExpense = document.getElementById("selectfield-expense");
+    // var selectTextExpense = document.getElementById("selecttext-expense");
+    // var optionsExpense = document.getElementsByClassName("options-expense");
+    // var listExpense = document.getElementById("list-expense");
 
-// selectFieldExpense.onclick = function () {
-// listExpense.classList.toggle("hide1");
-// }
+    // selectFieldExpense.onclick = function () {
+    // listExpense.classList.toggle("hide1");
+    // }
 
-// for (option of optionsExpense) {
-// option.onclick = function () {
-//     selectTextExpense.innerHTML = this.innerHTML;
-//     listExpense.classList.toggle("hide1");
-// };
-// }
+    // for (option of optionsExpense) {
+    // option.onclick = function () {
+    //     selectTextExpense.innerHTML = this.innerHTML;
+    //     listExpense.classList.toggle("hide1");
+    // };
+    // }
 
-// // FOR TRANSACTION TYPE DROPDOWN
-// var selectFieldTransaction = document.getElementById("selectfield-transaction");
-// var selectTextTransaction = document.getElementById("selecttext-transaction");
-// var optionsTransaction = document.getElementsByClassName("options-transaction");
-// var listTransaction = document.getElementById("list-transaction");
+    // // FOR TRANSACTION TYPE DROPDOWN
+    // var selectFieldTransaction = document.getElementById("selectfield-transaction");
+    // var selectTextTransaction = document.getElementById("selecttext-transaction");
+    // var optionsTransaction = document.getElementsByClassName("options-transaction");
+    // var listTransaction = document.getElementById("list-transaction");
 
-// selectFieldTransaction.onclick = function () {
-// listTransaction.classList.toggle("hide2");
-// }
+    // selectFieldTransaction.onclick = function () {
+    // listTransaction.classList.toggle("hide2");
+    // }
 
-// for (option of optionsTransaction) {
-// option.onclick = function () {
-//     selectTextTransaction.innerHTML = this.innerHTML;
-//     listTransaction.classList.toggle("hide2");
-// };
-// }
+    // for (option of optionsTransaction) {
+    // option.onclick = function () {
+    //     selectTextTransaction.innerHTML = this.innerHTML;
+    //     listTransaction.classList.toggle("hide2");
+    // };
+    // }
+};
 </script>
 
 <style>
---
+++ src/views/Login.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <body>
+        <div id="login" class="container">
+            <div class="header">
+                <div class="circle"></div>
+                <div class="circle1"></div>
+                <div class="circle2"></div>
+                <h3 class="Title">XPENSE TRACK</h3>
+            </div>
+
+            <div class="mainbody">
+                <div class="LoginBox">
+                    <h1 class="LoginText">Login</h1>
+                    <form method="post">
+                        <div class="txt_field">
+                            <input type="text" required>
+                            <span></span>
+                            <label>Email</label>
+                        </div>
+                        <div class="txt_field">
+                            <input type="password" required>
+                            <span></span>
+                            <label>Password</label>
+                        </div>
+                        <div class="ForgotPass">Forgot Password?
+
+                        </div>
+                        <router-link to="/home" style="text-decoration: none;color: inherit;">
+                            <input type="submit" value="Login">
+                        </router-link>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container #login {
+    width: 414px;
+    height: 896px;
+    margin: 0 auto;
+    position: relative;
+    background: linear-gradient(132deg, #63B5AF 0%, #438883 100%);
+    box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.2);
+    overflow: hidden;
+}
+
+.header {
+    height: 120px;
+    padding: 20px;
+    text-align: center;
+
+}
+
+.header h3{
+    color: #ffffff;
+    padding: 30px 30px;
+    font-size: xx-large;
+}
+
+input[type="submit"]{
+    width: 100%;
+    height: 50px;
+    border: 2px;
+    background: #438883;
+    border-radius: 25px;
+    font-size: 18px;
+    color: #ffffff;
+}
+</style>
